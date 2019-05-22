91fa0b3ac78e541c2b02581bbd7f4568dd273be6
PwnedCax <43464443+PwnedCax@users.noreply.github.com>
2019-05-22 14:48:15 +0200

    Update fr.lua
    add language fr

--- locales/fr.lua
+++ locales/fr.lua
@@ -3,9 +3,9 @@ Locales["fr"] = {
 	["black_money"] = "Argent sale",
 	["player_nearby"] = "Le joueur sélectionné n'est plus près de vous.",
 	["players_nearby"] = "Il n'y a personne près de vous.",
-	["openinv_help"] = "Opens inventory of another player",
-	["openinv_id"] = "Player ID",
-	["no_permissions"] = "You don't have permissions to do that!",
-	["no_player"] = "Didn't find player with given ID!",
-	["player_inventory"] = "Player inventory"
+	["openinv_help"] = "Ouvrir l'inventaire d'un autre joueur",
+	["openinv_id"] = "ID du joueur",
+	["no_permissions"] = "Vous n'avez pas les permissions pour faire ça !",
+	["no_player"] = "Je n'ai pas trouvé le joueur avec cet ID!",
+	["player_inventory"] = "Inventaire du joueur"
 }
